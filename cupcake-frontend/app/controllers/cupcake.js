20dd0d6365bd18294004cfea36983fa1e57caeae
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2017-01-15 20:02:03 +0200

    componentize user interface

--- cupcake-frontend/app/controllers/cupcake.js
+++ cupcake-frontend/app/controllers/cupcake.js
@@ -1,19 +1,4 @@
 import Ember from 'ember';
-import DS from 'ember-data';
 
 export default Ember.Controller.extend({
-  summaryRating: Ember.computed('model.@each.ratings', function(){
-    return DS.PromiseObject.create({
-      promise: this.get('model.ratings').then((ratings) =>{
-        if (Ember.isEmpty(ratings)){
-          return "No ratings for this cupcake, yet.";
-        }
-        let ratingVals = ratings.mapBy('value');
-        console.log("ratingsList: " + JSON.stringify(ratingVals));
-        let sum = ratingVals.reduce(function(a, b){ return a + b;}, 0);
-        console.log("sum " + sum);
-        return sum / ratingVals.length;
-      })
-    });
-  }).property('')
 });
--- cupcake-frontend/app/models/cupcake.js
+++ cupcake-frontend/app/models/cupcake.js
@@ -1,9 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   text: DS.attr('string'),
   weight: DS.attr('number'),
   ratings: DS.hasMany('rating'),
-  thumbnail: DS.attr('string')
+  thumbnail: DS.attr('string'),
+  hasRatings: Ember.computed.notEmpty('ratings'),
+  ratingVals: Ember.computed.mapBy('ratings', 'value'),
+  ratingSum: Ember.computed.sum('ratingVals'),
+  summaryRating: Ember.computed('ratingVals', 'hasRatings', function(){
+    return this.get('ratingSum') / this.get('ratingVals').length;
+  })
 });
